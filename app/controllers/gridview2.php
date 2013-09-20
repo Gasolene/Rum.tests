e5c6f66b3cbff0c3fb4c914fbb2027395777d8e5
Darnell <darnell@commerx.com>
2013-09-20 13:49:37 -0600

    Update Gridview2

--- app/controllers/gridview2.php
+++ app/controllers/gridview2.php
@@ -8,10 +8,9 @@
 			$this->page->form->add(\MyApp\Models\Customer::gridview('table'));
 			$this->table->showFilters = true;
 			$this->table->showFooter = true;
+			$this->table->pageSize=4;
 			$this->page->table->columns->ajaxPostBack = true;
 			$this->page->form->table->columns->add(new \System\Web\WebControls\GridViewButton('customer_id', 'Delete', 'action', 'Are you sure you want to delete?', '', '', 'action', 'Add'));
-
-			$this->page->table->columns->ajaxPostBack = true;
 		}
 
 		function onPageLoad($sender, $args) {
@@ -22,13 +21,15 @@
 		{
 			if($this->isAjaxPostBack)
 			{
+				\Rum::flash("Try AJAX Post");
+
 				if(isset(\System\Web\HTTPRequest::$post["customer_id"]))
 				{
 					$entity = \MyApp\Models\Customer::findById(\System\Web\HTTPRequest::$post["customer_id"]);
 
 					foreach($entity->fields as $field=>$type)
 					{
-						if(isset(\System\Web\HTTPRequest::$post[$this->formatField($field)]))
+						if(isset(\System\Web\HTTPRequest::$post[$field]))
 						{
 							$entity[$field] = \System\Web\HTTPRequest::$post[$field];
 						}
@@ -42,14 +43,20 @@
 
 		public function onActionPost($sender, $args)
 		{
+			\Rum::flash("Try Action on Record #{$args["action"]}");
+
 			if((int)$args["action"])
 			{
+				\Rum::flash("Try Delete Action on Record #{$args["action"]}");
+
 				// Delete
 				$this->table->deleteRow($args["action"]);
-				\Rum::flash("s:Record #{$args["action"]} deleted");
+				\Rum::flash("s:Customer #{$args["action"]} deleted");
 			}
 			else
 			{
+				\Rum::flash("Try Insert Action");
+
 				// Insert
 				$this->table->insertRow();
 				\Rum::flash("s:Customer {$this->table->dataSource["customer_id"]} added");
@@ -57,21 +64,14 @@
 			\Rum::forward();
 		}
 
-		public function onActionAjaxPost($sender, $args)
+		public function xonActionAjaxPost($sender, $args)
 		{
-			if((int)$args["action"])
-			{
-				// Delete
-				$this->table->deleteRow($args["action"]);
-				\Rum::flash("s:AJAXRecord #{$args["action"]} deleted");
-			}
-			else
-			{
-				// Insert
-				$this->table->insertRow();
-				\Rum::flash("s:AJAXCustomer {$this->table->dataSource["customer_id"]} added");
-			}
-			\Rum::forward();
+			\Rum::flash("Try AJAX Delete Record #{$args["customer_id"]}");
+
+			// Ajax Delete
+			$this->table->deleteRow($args["customer_id"]);
+			\Rum::flash("s:Customer #{$args["customer_id"]} has been deleted");
+			$this->table->updateAjax();
 		}
 	}
 ?>
